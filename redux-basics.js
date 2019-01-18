6e2a544bd04eafb16c7b2e0fd33d16fe291760db
Mihai Enuica <mihai.enuica@gmail.com>
2019-01-18 19:56:13 +0200

    S14 - ProjReduxOne #2 - Dispatching Actions //	modified:   redux-basics.js

--- redux-basics.js
+++ redux-basics.js
@@ -12,6 +12,22 @@ const initialState = {
 // a reducer is a function with two arguments;  state and action
 // in this example we defined the state default value
 const rootReducer = (state = initialState, action ) => {
+  if(action.type === 'INC_COUNTER'){
+    // always make a copy of the state and change the value
+    return {
+      ...state,
+      // here action is only a number
+      counter: state.counter + 1
+    }
+  }
+  if(action.type === 'ADD_COUNTER'){
+    // always make a copy of the state and change the value
+    return {
+      ...state,
+      // here we have a value property of the action
+      counter: state.counter + action.value
+    }
+  }
   return state;
 }
 
@@ -21,5 +37,13 @@ const store = createStore(rootReducer);
 console.log('store.getState(): ', store.getState());
 
 // Dispatching Action
+// type is important to be one key of the JS object as parameter for dispatch
+// the value of type is by convention with capital letters
+// store.dispatch(action)
+store.dispatch({type: 'INC_COUNTER'});
+// here action object has two properties type, and value
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+
+console.log('after store.dispatch(): store.getState()', store.getState());
 
 // Subscription
